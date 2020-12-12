03daee41e24787d9b369bc4889b2907adfa63665
Rinco1000 <a.rinco@outllok.com>
2020-12-12 18:46:12 -0300

    save files

---
+++ aula/Main.java
@@ -0,0 +1,35 @@
+package aula;
+
+public class Main {
+	public static void main(String[] args) {
+		/*
+		int age = 33;
+		if (age < 18) {
+			System.out.println("Voce nao pode entrar na festa");
+			
+		}else {
+			System.out.println("Voce pode entrar na festa");
+		}
+		*/
+		
+		/*
+		int restart = 1;
+		while (restart <= 5) {
+			System.out.println("Restart in: " + restart + " seconds");
+			restart++;
+		}
+		*/
+		/*
+		int life_1 = 50;
+		int life_2 = 25;
+		int life_3 = 25;
+		 
+		int final_life = life_1 + life_2 + life_3;
+		System.out.println(final_life);
+		*/
+	     
+		for ( int restart = 1; restart <= 5; restart++) {
+			System.out.println("Restart in: " + restart + " seconds");
+		}
+	}
+}
