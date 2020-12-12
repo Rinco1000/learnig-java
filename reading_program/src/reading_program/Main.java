85f1e6e6a7f9e13f7afcd295373df53b2778133b
Rinco1000 <a.rinco@outllok.com>
2020-12-12 19:59:50 -0300

    save files

---
+++ reading_program/src/reading_program/Main.java
@@ -0,0 +1,36 @@
+package reading_program;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) {
+		
+		/*
+		String nome, idade, peso, cidade, pais;
+		System.out.println("Por favor digite seu nome");
+		 nome = in.nextLine();
+		System.out.println("Por favor digite sua idade");
+		idade = in.nextLine();
+		System.out.println("Por favor digite seu peso");
+	    peso = in.nextLine();
+		System.out.println("Por favor digite sua cidade");
+	    cidade = in.nextLine();
+		System.out.println("Por favor digite seu país");
+	    pais = in.nextLine();
+		
+		System.out.println("Nome: " + nome );
+		System.out.println("idade: " + idade );
+		System.out.println("peso: " + peso );
+		System.out.println("cidade: " + cidade );
+		System.out.println("pais: " + pais );
+        */
+		Scanner in = new Scanner(System.in);
+		System.out.println("Digite sua idade");
+	    String idade = in.nextLine();
+		
+			
+		
+		
+		
+	}
+}
